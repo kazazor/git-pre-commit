b400a6cdfa8c443bda735490fe3c4b5141e66046
Or Kazaz <or.kazaz89@gmail.com>
2016-02-27 15:43:18 +0200

    Added jsdocs tot he GitManager class

--- utils/git-manager.js
+++ utils/git-manager.js
@@ -4,6 +4,11 @@
 
 var execSync = require('child_process').execSync;
 
+/**
+ * Gets the project's git root directory
+ * @function
+ * @return {string} the git root directory
+ */
 function getGitRootDirectory() {
   try {
     return execSync('git rev-parse --show-toplevel').toString().trim();
@@ -12,8 +17,19 @@ function getGitRootDirectory() {
   }
 }
 
+/**
+ * The constructor for the GitManager class.
+ * By default sets the gitRootDirectory field.
+ * @constructor
+ */
 function GitManager() {
   this.gitRootDirectory = getGitRootDirectory();
 }
+
+/**
+ * The git root directory path
+ * @type {string}
+ */
+GitManager.prototype.gitRootDirectory = undefined;
 
 module.exports = new GitManager();
