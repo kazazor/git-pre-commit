a560fee58d13d17581e3cf446e8f96429b34a6e9
Or Kazaz <or.kazaz89@gmail.com>
2016-07-02 15:38:40 +0300

    Fixed the issue where when running git commit --amend we no commits you'll get an error (#21)

--- scripts/pre-commit.js
+++ scripts/pre-commit.js
@@ -32,6 +32,18 @@ function execCommand(command, root) {
 }
 
 function restoreTree() {
+  /**
+  Let's consider this case:
+  You have a lint task that runs on pre-commit hook.
+  You're modifying the file x.js and you git add it.
+  Now you left to do something else and didn't commit yet.
+  You then return to this file and perform more changes that should not be part of the commit on x.js.
+  You're trying to commit the stagged changes you git add and your lint task fails the commit.
+  If you haven't restored the tree what will happen is the stash pop command will try to auto merge x.js and then
+  it will have the unstagged changes now be in the stagged changes - which is something you did not want.
+
+  So running git reset --hard command is to take care of situations where you have the same files in the stagged and unstagged changes.
+   */
   var command = "git reset --hard";
   execCommand(command, gitRoot);
 
@@ -61,17 +73,23 @@ if (!gitRoot) {
     exit(1);
   } else {
     var command;
+    var hasChanges = gitManager.hasChanges();
 
-    // Ensure that code that isn't part of the prospective commit isn't tested within your pre-commit script
-    command = "git stash --quiet --keep-index --include-untracked";
-    execCommand(command, gitRoot);
+    // If we do not have any changes on local git we do not need to stash.
+    // This could happen when the user runs 'git commit --amend' when there are no commits cause he just wanted
+    // to change the message that he typed in the last commit
+    if (hasChanges) {
+      // Ensure that code that isn't part of the prospective commit isn't tested within your pre-commit script
+      command = "git stash --quiet --keep-index --include-untracked";
+      execCommand(command, gitRoot);
+    }
 
     var packageJson = JSON.parse(fs.readFileSync(path.join(gitRoot, 'package.json')));
 
     // Checks if the command to run exists in the package.json file
     if (!packageJson.precommit) {
       gulpUtils.print("You did not supply any code to run in the 'precommit' field in the package.json file", {color: 'red'});
-      exit(1, true);
+      exit(1, hasChanges);
     } else {
       var commandParts = packageJson.precommit.split(" ");
 
@@ -88,7 +106,7 @@ if (!gitRoot) {
           exitCode = 1;
         }
 
-        exit(exitCode, true);
+        exit(exitCode, hasChanges);
       });
     }
   }
